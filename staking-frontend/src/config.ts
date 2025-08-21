257d0fdaaec18ac58d4469dfc2b8799406d65aaa
Asif Ali <aali9548702@gmail.com>
2025-08-21 23:56:45 +0530

    completed

--- staking-frontend/src/config.ts
+++ staking-frontend/src/config.ts
@@ -11,7 +11,7 @@ export const config = createConfig({
   connectors: [injected()],
   chains: [mainnet, sepolia],    // this mainnet is the ethereum mainnet
   transports: {
-    [mainnet.id]: http("https://eth-mainnet.g.alchemy.com/v2/_oKVscmqHn_W3br1MYD_Xesg3ZUmjlWO"),
-    [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/_oKVscmqHn_W3br1MYD_Xesg3ZUmjlWO"),
+    [mainnet.id]: http(import.meta.env.MAINNET_ID),
+    [sepolia.id]: http(import.meta.env.SEPOLIA_ID),
   },
 })
